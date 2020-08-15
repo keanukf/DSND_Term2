9765acdf23ffa32f031ec4580e45f7de31ba387d
Keanu <forthmann@gmx.net>
2020-08-16 00:56:14 +0200

    Changed graphs
    Changed graphs to a dummy NYT API Chart to check connection

--- README.md
+++ README.md
@@ -1,29 +1,27 @@
-# Link to Webapp [https://world-bank-dashboard-api.herokuapp.com](https://world-bank-dashboard-api.herokuapp.com)
+# Link to Webapp [https://udacity-venture-recommendation.herokuapp.com/](https://udacity-venture-recommendation.herokuapp.com/)
 
-# World Bank API Data Dashboard 
+# Digital Health Venture Dashboard
 
-This is a flask app that visualizes data from the world bank API. Data is
-pulled directly from the API and then visualized using Plotly.
+This is a flask app that visualizes a set of data including digital health startups. Data is visualized using Plotly.
+This is a project in development for the Udacity Data Scientist Nanodegree.
 
-This is a project in development for the Udacity Data Scientist Nanodegree. 
-
-## Getting Started 
+## Getting Started
 
 This flask app can be used as a template for visualizing your own data. Use
-the template to enhance your professional portfolio. 
+the template to enhance your professional portfolio.
 
 ## Prerequisites
 
 To install the flask app, you need:
 - python3
 - python packages in the requirements.txt file
- 
+
  Install the packages with
-``` 
+```
  pip install -r requirements.txt
 ```
 
 ## Installing
 
-On a MacOS/linux system, installation is easy. Open a terminal, and go into 
-the directory with the flask app files. Run `python worldbank.py` in the terminal.
+On a MacOS/linux system, installation is easy. Open a terminal, and go into
+the directory with the flask app files. Run `python digitalhealth.py` in the terminal.
--- scripts/data.py
+++ scripts/data.py
@@ -5,177 +5,119 @@ import plotly.colors
 from collections import OrderedDict
 import requests
 
-
-# default list of all countries of interest
-country_default = OrderedDict([('Canada', 'CAN'), ('United States', 'USA'), 
-  ('Brazil', 'BRA'), ('France', 'FRA'), ('India', 'IND'), ('Italy', 'ITA'), 
-  ('Germany', 'DEU'), ('United Kingdom', 'GBR'), ('China', 'CHN'), ('Japan', 'JPN')])
-
-
-def return_figures(countries=country_default):
-  """Creates four plotly visualizations using the World Bank API
-
-  # Example of the World Bank API endpoint:
-  # arable land for the United States and Brazil from 1990 to 2015
-  # http://api.worldbank.org/v2/countries/usa;bra/indicators/AG.LND.ARBL.HA?date=1990:2015&per_page=1000&format=json
-
-    Args:
-        country_default (dict): list of countries for filtering the data
+def return_figures():
+  """ Creates four plotly visualizations using the New York Times Archive API
 
     Returns:
         list (dict): list containing the four plotly visualizations
-
   """
+  # Add New York Times API Key
+  nyt = NYTAPI("AsjeHhqDYrePA2GMPpYoY1KAKAdG7P99")
 
-  # when the countries variable is empty, use the country_default dictionary
-  if not bool(countries):
-    countries = country_default
+  # Select Year and Month of articles
+  data = nyt.archive_metadata(
+    date = datetime.datetime(2020, 7, 1)
+    )
 
-  # prepare filter data for World Bank API
-  # the API uses ISO-3 country codes separated by ;
-  country_filter = list(countries.values())
-  country_filter = [x.lower() for x in country_filter]
-  country_filter = ';'.join(country_filter)
+  def data_to_df(data):
+      # Initiate list for restructured information
+      data_list = []
 
-  # World Bank indicators of interest for pulling data
-  indicators = ['AG.LND.ARBL.HA.PC', 'SP.RUR.TOTL.ZS', 'SP.RUR.TOTL.ZS', 'AG.LND.FRST.ZS']
+      # Collect Data from API dictionary
+      for article in data:
+          new_data = [article.get("section_name"),
+                     article.get("news_desk"),
+                     article.get("pub_date"),
+                     article.get("headline").get("main"),
+                     article.get("abstract"),
+                     article.get("lead_paragraph"),
+                     article.get("type_of_material"),
+                     article.get("word_count")]
+          # Append list of information from article to data list
+          data_list.append(new_data)
 
-  data_frames = [] # stores the data frames with the indicator data of interest
-  urls = [] # url endpoints for the World Bank API
+      # Convert data list to DataFrame
+      df = pd.DataFrame(data_list, columns=["section_name","news_desk", "pub_date", "headline", "abstract", "lead_paragraph", "type_of_material", "word_count"])
 
-  # pull data from World Bank API and clean the resulting json
-  # results stored in data_frames variable
-  for indicator in indicators:
-    url = 'http://api.worldbank.org/v2/countries/' + country_filter +\
-    '/indicators/' + indicator + '?date=1990:2015&per_page=1000&format=json'
-    urls.append(url)
+      return df
 
-    try:
-      r = requests.get(url)
-      data = r.json()[1]
-    except:
-      print('could not load data ', indicator)
+  df = data_to_df(data)
 
-    for i, value in enumerate(data):
-      value['indicator'] = value['indicator']['value']
-      value['country'] = value['country']['value']
-
-    data_frames.append(data)
-  
-  # first chart plots arable land from 1990 to 2015 in top 10 economies 
-  # as a line chart
+  # first chart plots section distribution
+  # as a pie chart
   graph_one = []
-  df_one = pd.DataFrame(data_frames[0])
+  df_one = df.copy()
 
   # filter and sort values for the visualization
-  # filtering plots the countries in decreasing order by their values
-  df_one = df_one[(df_one['date'] == '2015') | (df_one['date'] == '1990')]
-  df_one.sort_values('value', ascending=False, inplace=True)
+  # filtering plots the articles in decreasing order by their values
+  labels = df_one.section_name.value_counts().index
+  values = df_one.section_name.value_counts().values
 
-  # this  country list is re-used by all the charts to ensure legends have the same
-  # order and color
-  countrylist = df_one.country.unique().tolist()
-  
-  for country in countrylist:
-      x_val = df_one[df_one['country'] == country].date.tolist()
-      y_val =  df_one[df_one['country'] == country].value.tolist()
-      graph_one.append(
-          go.Scatter(
-          x = x_val,
-          y = y_val,
-          mode = 'lines',
-          name = country
-          )
-      )
-
-  layout_one = dict(title = 'Change in Hectares Arable Land <br> per Person 1990 to 2015',
-                xaxis = dict(title = 'Year',
-                  autotick=False, tick0=1990, dtick=25),
-                yaxis = dict(title = 'Hectares'),
-                )
-
-  # second chart plots ararble land for 2015 as a bar chart
-  graph_two = []
-  df_one.sort_values('value', ascending=False, inplace=True)
-  df_one = df_one[df_one['date'] == '2015'] 
-
-  graph_two.append(
-      go.Bar(
-      x = df_one.country.tolist(),
-      y = df_one.value.tolist(),
-      )
+  graph_one.append(
+    go.Pie(
+        labels=labels,
+        values=values
+    )
   )
 
-  layout_two = dict(title = 'Hectares Arable Land per Person in 2015',
-                xaxis = dict(title = 'Country',),
-                yaxis = dict(title = 'Hectares per person'),
-                )
+  layout_one = dict(title = 'Distribution of sections of this months New York Times articles')
 
-  # third chart plots percent of population that is rural from 1990 to 2015
+  # second chart plots section distribution
+  # as a pie chart
+  graph_two = []
+  df_two = df.copy()
+
+  # filter and sort values for the visualization
+  # filtering plots the articles in decreasing order by their values
+  labels = df_two.section_name.value_counts().index
+  values = df_two.section_name.value_counts().values
+
+  graph_two.append(
+    go.Pie(
+        labels=labels,
+        values=values
+    )
+  )
+
+  layout_two = dict(title = 'Distribution of sections of this months New York Times articles')
+
+  # third chart plots section distribution
+  # as a pie chart
   graph_three = []
-  df_three = pd.DataFrame(data_frames[1])
-  df_three = df_three[(df_three['date'] == '2015') | (df_three['date'] == '1990')]
+  df_three = df.copy()
 
-  df_three.sort_values('value', ascending=False, inplace=True)
-  for country in countrylist:
-      x_val = df_three[df_three['country'] == country].date.tolist()
-      y_val =  df_three[df_three['country'] == country].value.tolist()
-      graph_three.append(
-          go.Scatter(
-          x = x_val,
-          y = y_val,
-          mode = 'lines',
-          name = country
-          )
-      )
+  # filter and sort values for the visualization
+  # filtering plots the articles in decreasing order by their values
+  labels = df_three.section_name.value_counts().index
+  values = df_three.section_name.value_counts().values
 
-  layout_three = dict(title = 'Change in Rural Population <br> (Percent of Total Population)',
-                xaxis = dict(title = 'Year',
-                  autotick=False, tick0=1990, dtick=25),
-                yaxis = dict(title = 'Percent'),
-                )
+  graph_three.append(
+    go.Pie(
+        labels=labels,
+        values=values
+    )
+  )
 
-  # fourth chart shows rural population vs arable land as percents
+  layout_three = dict(title = 'Distribution of sections of this months New York Times articles')
+
+  # fourth chart plots section distribution
+  # as a pie chart
   graph_four = []
-  df_four_a = pd.DataFrame(data_frames[2])
-  df_four_a = df_four_a[['country', 'date', 'value']]
-  
-  df_four_b = pd.DataFrame(data_frames[3])
-  df_four_b = df_four_b[['country', 'date', 'value']]
+  df_four = df.copy()
 
-  df_four = df_four_a.merge(df_four_b, on=['country', 'date'])
-  df_four.sort_values('date', ascending=True, inplace=True)
+  # filter and sort values for the visualization
+  # filtering plots the articles in decreasing order by their values
+  labels = df_four.section_name.value_counts().index
+  values = df_four.section_name.value_counts().values
 
-  plotly_default_colors = plotly.colors.DEFAULT_PLOTLY_COLORS
+  graph_four.append(
+    go.Pie(
+        labels=labels,
+        values=values
+    )
+  )
 
-  for i, country in enumerate(countrylist):
-
-      current_color = []
-
-      x_val = df_four[df_four['country'] == country].value_x.tolist()
-      y_val = df_four[df_four['country'] == country].value_y.tolist()
-      years = df_four[df_four['country'] == country].date.tolist()
-      country_label = df_four[df_four['country'] == country].country.tolist()
-
-      text = []
-      for country, year in zip(country_label, years):
-          text.append(str(country) + ' ' + str(year))
-
-      graph_four.append(
-          go.Scatter(
-          x = x_val,
-          y = y_val,
-          mode = 'lines+markers',
-          text = text,
-          name = country,
-          textposition = 'top'
-          )
-      )
-
-  layout_four = dict(title = '% of Population that is Rural versus <br> % of Land that is Forested <br> 1990-2015',
-                xaxis = dict(title = '% Population that is Rural', range=[0,100], dtick=10),
-                yaxis = dict(title = '% of Area that is Forested', range=[0,100], dtick=10),
-                )
+  layout_four = dict(title = 'Distribution of sections of this months New York Times articles')
 
 
   # append all charts
